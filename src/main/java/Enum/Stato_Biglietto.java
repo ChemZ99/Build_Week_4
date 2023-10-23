709a56fb802adb13d2ee29c356f3111a11986fcd
ManueleSeretti <136794705+ManueleSeretti@users.noreply.github.com>
2023-10-23 14:50:10 +0200

    creazione enum

---
+++ src/main/java/Enum/Stato_Biglietto.java
@@ -0,0 +1,16 @@
+package Enum;
+
+import java.util.Random;
+
+public enum Stato_Biglietto {
+    VITIMATO,
+    UTILIZZABILE;
+    private static final Random rndm = new Random();
+
+    public static Stato_Biglietto randomPeriodic() {
+
+        Stato_Biglietto[] stato = values();
+        return stato[rndm.nextInt(stato.length)];
+
+    }
+}
---
+++ src/main/java/Enum/Stato_Distributore.java
@@ -0,0 +1,16 @@
+package Enum;
+
+import java.util.Random;
+
+public enum Stato_Distributore {
+    ATTIVO,
+   FUORI_SERVIZIO;
+    private static final Random rndm = new Random();
+
+    public static Stato_Distributore randomPeriodic() {
+
+        Stato_Distributore[] stato = values();
+        return stato[rndm.nextInt(stato.length)];
+
+    }
+}
