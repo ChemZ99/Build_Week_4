bdbea9a9421a5ae13fa4cd7944e57b0c9a9ac0a0
Andrea Cavallo <astarore@gmail.com>
2023-10-23 13:00:34 +0200

    project initialization

---
+++ src/main/java/Utils/JpaUtil.java
@@ -0,0 +1,20 @@
+package Utils;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaUtil {
+    private static final EntityManagerFactory entityManagerFactory;
+
+    static {
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("BUILD_WEEK_4");
+        } catch (Throwable ex) {
+            System.err.println("Initial EntityManagerFactory creation failed" + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    public static EntityManagerFactory getEntityManagerFactory() {
+        return entityManagerFactory;
+    }
+}
